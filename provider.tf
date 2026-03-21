d59aacfa232e596f6494cedc8969649c523387fd
Vaibhav Tiwari <techdevopsvaibhav03@gmail.com>
2026-03-21 19:09:55 +0530

    first commit: created file main,varibales,values & provider

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">=1.4"
+  required_providers {
+    google = {
+        version = ">=5.0"
+        source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+}
---
+++ values.tfvars
@@ -0,0 +1,2 @@
+project_id = "apt-gear-359813"
+region = "us-central1 "
